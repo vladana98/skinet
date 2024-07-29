d738045f854a6ee249f6e596c81dc44d0ed86872
vladana98 <vladanaveselinovic@yahoo.com>
2024-07-29 12:16:44 +0200

    prvi commit

---
+++ API/Entities/Product.cs
@@ -0,0 +1,11 @@
+using System.Runtime.CompilerServices;
+
+namespace Core.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
